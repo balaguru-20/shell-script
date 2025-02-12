32d807863b38006d679e604b80b5ac11cebe54af
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-12 23:42:41 +0530

    conversation updated

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Rahim: Hi Rabort"
-echo "Robert: Hello Rahim"
-echo "Rahim: How are you doing"
-echo "Robert: I am good. How are you?"
+
+
+echo "Rahim:: Hi Rabort"
+echo "Robert:: Hello Rahim"
+echo "Rahim:: How are you doing"
+echo "Robert:: I am good. How are you?"
