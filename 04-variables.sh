80ccefc9e8ebc2ebb8d4024e90012dffb5ea9ce2
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-13 00:05:54 +0530

    conversation updated

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,5 +5,5 @@ PERSON2=$2
 
 echo "$PERSON1:: Hi $PERSON2"
 echo "$PERSON2:: Hello $PERSON1"
-echo "${PERSON1}:: How are you doing" # → We can give ${PERSON1} like this as well
+echo "${PERSON1}:: How are you doing" 
 echo "$PERSON2:: I am good. How are you?"
---
+++ 05-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "please enter your username::" #the text entered here will be added as value to the varibale
+
+read USERNAME # Here USERNAME is the variable name
+
+echo "username entered is : $USERNAME"
