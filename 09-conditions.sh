5388f24ed7268a973ef552d09b507f39cc6218c1
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-20 20:17:08 +0530

    Conditions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+echo "Enter the Number you want"
 read NUMBER
 echo "Entered number is $NUMBER"
 
-if [ $NUMBER -le 100 ]
+if [ $NUMBER -le 100 ]      # -lt, -gt, -le, -ge, -eq
 then
     echo "$NUMBER is less than or equal to 100"
 else
