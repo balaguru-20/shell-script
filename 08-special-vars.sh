08cb99c5639b5b2f968b1a0ca599f6fd392b14d9
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-20 19:45:59 +0530

    Specail variabls

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,5 +7,5 @@ echo "Present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo "Which user is running this script: $USER"
 echo "Process id of current script: $$"
-sleep 60 &
+sleep 60 &      # '&' means it runs in the background
 echo "Process id of last command in background: $!"
