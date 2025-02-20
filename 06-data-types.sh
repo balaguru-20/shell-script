a2d0e73fca804c0f26e1e41b8efaa5f13d2490b8
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-20 18:05:07 +0530

    Adding 2 elements

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -5,6 +5,6 @@ NUMBER2=$2
 
 TIMESTAMP=$(date)
 echo "script excuted at: $TIMESTAMP"
-WORD=$TIMESTAMP | grep Thu
+WORD= $cat TIMESTAMP | grep Thu
 SUM=$(($NUMBER1+$NUMBER2))
 echo "sum of NUMBER1 and NUMBER2 is: $SUM $WORD"
