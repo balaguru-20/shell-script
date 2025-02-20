a3e897abad0ad2b00d169127e178695c7e884f94
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-20 18:23:36 +0530

    Adding 2 elements

---
+++ 07-arrays.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+MOVIES=("pushpa" "devara" "rrr")
+#Array's index starts from '0'. Size is 3
+
+echo "First movie is ${MOVIES[0]}"
+echo "Second movie is ${MOVIES[1]}"
+echo "Third movie is ${MOVIES[2]}"
+
+echo "All movies are ${MOVIES[@]}"  # '@' means ALL
+echo "Movies names are: $MOVIES"
+echo "The second movie is NNTR's $MOVIES[1]" 
