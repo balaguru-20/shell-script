cb42d68343bbfdad9141e0f89dcd3485819d1430
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-20 18:26:21 +0530

    Adding 2 elements

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -8,5 +8,5 @@ echo "Second movie is ${MOVIES[1]}"
 echo "Third movie is ${MOVIES[2]}"
 
 echo "All movies are ${MOVIES[@]}"  # '@' means ALL
-echo "Movies names are: $MOVIES"
-echo "The second movie is NNTR's $MOVIES[1]" 
+echo "Movies names are: ${MOVIES}"
+echo "The second movie is NNTR's ${MOVIES[1]}" 
