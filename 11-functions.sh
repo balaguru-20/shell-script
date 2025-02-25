468b26184176b2d5e56e977b5f9d0bc81fc98fb9
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-25 14:49:25 +0530

    Functions script

---
+++ 11-functions.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]     # '$?' means checking previous command status success or not if it is '0' success. Otherwise failure
+    then
+        echo "$2 FAILURE"
+        exit 1
+    else
+        echo "$2 SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute this script"
+    exit 1      #Other than 0
+fi
+
+dnf list installed mysql    # checking installed or not
+
+if  [ $? -ne 0]     # checking installed or not
+then
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already ... INSTALLED"
+fi
+
+dnf list installed git  # checking installed or not
+
+if [ $? -ne 0 ]     # checking installed or not
+then
+    dnf install git -y
+    VALIDATE $? "Insatlling Git"
+else
+    echo "Git is already ... INSTALLED"
+fi
