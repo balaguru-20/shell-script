667fc375a36c6fbec76eddcf015218d425bd75d9
balaguru-20 <balaguravaiah0606@gmail.com>
2025-02-28 14:02:37 +0530

    logs

---
+++ 13-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"  #Red
+G="\e[32m"  #Green
+Y="\e[33m"  #Yellow
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cur -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){             #We can write the function anywhere in the program
+    if [ $1 -ne 0 ]    
+    then
+        echo -e "$2 ... $R FAILURE" #-e means enable, $R color cpplying
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS"
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R ERROR:: you must have sudo access to execute this script"
+    exit 1      #Other than 0
+fi
+
+dnf list installed mysql  &>>$LOG_FILE  # checking installed or not
+
+if  [ $? -ne 0 ]     # checking installed or not
+then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "MySQL is already ... $Y INSTALLED"
+fi
+
+dnf list installed git  # checking installed or not
+
+if [ $? -ne 0 ]     # checking installed or not
+then
+    dnf install git -y &>>$LOG_FILE
+    VALIDATE $? "Insatlling Git"
+else
+    echo -e "Git is already ... $Y INSTALLED"
+fi
