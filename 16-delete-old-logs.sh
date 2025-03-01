9624f1024534c2ba9adfb5127c6b6fe987c304d0
balaguru-20 <balaguravaiah0606@gmail.com>
2025-03-01 19:08:27 +0530

    deleting old logs through shell script

---
+++ 16-delete-old-logs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"  #Red
+G="\e[32m"  #Green
+Y="\e[33m"  #Yellow
+N="\e[0m"   #Normal
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+LOGS_FOLDER="/var/log/shellscript-logs" # Path in the linux
+LOG_FILE=$(echo $0 | cut -d "." -f1 )   # It wll takes 13-logs.sh file name before .(dot)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){             #We can write the function anywhere in the program
+    if [ $1 -ne 0 ]   
+    then
+        echo -e "$2 ... $R FAILURE $N" #-e means enable, $R color cpplying
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e " $R ERROR:: you must have sudo access to execute this script $N"
+        exit 1      #Other than 0
+    fi
+    }
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14 )
+echo "Files to be deleted: $FILES_TO_DELETE"
