9cbb3b053f1bcb2f412e141130c01e5414286f66
balaguru-20 <balaguravaiah0606@gmail.com>
2025-03-06 19:36:53 +0530

    disk usage by shell script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,6 +2,7 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real projects will monitor for 70%
+MSG=""
 
 while read -r line
 do
@@ -10,9 +11,10 @@ do
     #echo "Partition: $PARTITION , Usage: $USAGE "
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="High disk usage on partion: $PARTITION, Usage is: $USAGE \n"
+        MSG+="High disk usage on partion: $PARTITION Usage is: $USAGE \n" # before = to '+' means it will append the new lines otherwise it will replace the new lines in the loop
     fi
 
 done <<< $DISK_USAGE
 
-echo -e "Message: $MSG"
+echo -e "Message: $MSG" #-e means it's enables the new line which we mention in the loop MSG variable as \n
+echo "guru $PARTITION"
