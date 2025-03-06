f603ef1743c75ecfbe9f2ff51feb9c2994be8690
balaguru-20 <balaguravaiah0606@gmail.com>
2025-03-06 19:27:31 +0530

    disk usage by shell script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -7,6 +7,12 @@ while read -r line
 do
     USAGE=$(echo $line |awk -F " " '{print $6F}' |cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partition: $PARTITION , Usage: $USAGE "
+    #echo "Partition: $PARTITION , Usage: $USAGE "
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High disk usage on partion: $PARTITION, Usage is: $USAGE /n"
+    fi
 
 done <<< $DISK_USAGE
+
+echo -e "Message: $MSG"
